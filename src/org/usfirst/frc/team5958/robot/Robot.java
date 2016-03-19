4fdf955b3340c7a6eca83790a0f7872ed81ef77e
zzagorsk <zzagorsk@cs.brown.edu>
2016-03-19 17:45:16 -0400

    Saturday's code

--- src/org/usfirst/frc/team5958/robot/Robot.java
+++ src/org/usfirst/frc/team5958/robot/Robot.java
@@ -6,6 +6,7 @@ import org.usfirst.frc.owatonnarobotics.input.XboxController;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.VictorSP;
 
@@ -22,6 +23,8 @@ public class Robot extends IterativeRobot {
   private Victor backLeft;
   private VictorSP frontRight;
   private VictorSP backRight;
+  private Spark ballPickup;
+  private Victor arm;
 
   private Joystick leftStick;
   private Joystick rightStick;
@@ -29,14 +32,19 @@ public class Robot extends IterativeRobot {
 
   private RobotDrive drive;
 
+  private int angle;
+
   private final int FRONTLEFT = 0;
   private final int BACKLEFT = 1;
   private final int FRONTRIGHT = 2;
   private final int BACKRIGHT = 3;
+  private final int SPARK = 4;
+  private final int ARM = 5;
 
   private final int LEFTSTICK = 0;
   private final int RIGHTSTICK = 1;
   private final int XBOX = 2;
+  private int autoLoopCounter;
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -45,9 +53,16 @@ public class Robot extends IterativeRobot {
   @Override
   public void robotInit() {
     this.backLeft = new Victor(this.BACKLEFT);
+    this.backLeft.setInverted(true);
     this.backRight = new VictorSP(this.BACKRIGHT);
+    this.backRight.setInverted(true);
     this.frontLeft = new Victor(this.FRONTLEFT);
+    this.frontLeft.setInverted(true);
     this.frontRight = new VictorSP(this.FRONTRIGHT);
+    this.frontRight.setInverted(true);
+    this.ballPickup = new Spark(this.SPARK);
+    this.arm = new Victor(this.ARM);
+    this.arm.setInverted(true);
 
     this.leftStick = new Joystick(this.LEFTSTICK);
     this.rightStick = new Joystick(this.RIGHTSTICK);
@@ -69,7 +84,8 @@ public class Robot extends IterativeRobot {
 
   @Override
   public void autonomousInit() {
-
+    this.autoLoopCounter = 0;
+    this.angle = 0;
   }
 
   /**
@@ -77,7 +93,12 @@ public class Robot extends IterativeRobot {
    */
   @Override
   public void autonomousPeriodic() {
-
+    if (this.autoLoopCounter < 1000) {
+      this.drive.drive(-0.7D, 0.0D);
+      this.autoLoopCounter += 1;
+    } else {
+      this.drive.drive(0.0D, 0.0D);
+    }
   }
 
   /**
@@ -86,6 +107,26 @@ public class Robot extends IterativeRobot {
   @Override
   public void teleopPeriodic() {
     this.drive.tankDrive(this.leftStick, this.rightStick);
+    if (this.xbox.y.get() && this.angle >= 0) {
+      this.arm.set(1);
+      this.angle -= 2;
+    } else if (this.xbox.a.get() && this.angle < 80) {
+      this.arm.set(-0.5);
+      this.angle++;
+    } else if (this.xbox.x.get() && this.xbox.b.get()) {
+      this.arm.set(1);
+      this.angle = 0;
+    } else {
+      this.arm.set(0);
+    }
+
+    if (this.xbox.lb.get() || this.xbox.rb.get()) {
+      this.ballPickup.set(0.5);
+    } else if (this.xbox.lt.get() || this.xbox.rt.get()) {
+      this.ballPickup.set(-0.5);
+    } else {
+      this.ballPickup.set(0);
+    }
   }
 
   /**
